2b770ccf426c7274caccfd3495ec736fe7495003
jane <janee1809xx@gmail.com>
2023-01-08 03:51:13 +0800

    update reviews route

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from routes.auth import auth
 from routes.account import account
 from models.products.Inventorybackend import CreateNewProduct
 from models.products.Product import Product
+from models.reviews.Review import Review
 
 # app initialization
 app = Flask(__name__)
@@ -147,21 +148,25 @@ def deleteProduct(id):
 
 @app.route('/reviews')
 def reviews():
+    return render_template('reviews/reviews.html')
+
+@app.route('/reviewsList')
+def reviewsList():
     reviews_list = []
     try:
         reviews_dict = {}
-        with shelve.open('reviews.db','r') as db:
-            if 'reviews' in db:
-                reviews_dict = db['reviews']
+        with shelve.open('review.db', 'r') as db:
+            if 'Reviews' in db:
+                reviews_dict = db['Reviews']
             for key in reviews_dict:
                 review = reviews_dict.get(key)
                 reviews_list.append(review)
     except IOError as ex:
         print(f"Error in retrieving reviews from review.db - {ex}")
     except Exception as ex:
-        print(f"Unknown error in retrieving customers from customer.db - {ex}")
+        print(f"Unknown error in retrieving reviews from review.db - {ex}")
 
-    return render_template('reviews/reviews.html')
+    return render_template('reviews/reviewsList.html', count=len(reviews_list), reviews_list=reviews_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
