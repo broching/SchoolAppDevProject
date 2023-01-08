83313de33a31f3cef3e4afcdbee2b61feb19a99c
charmainnn <chualewencharmain@gmail.com>
2023-01-09 02:15:35 +0800

    Updated Services forms

--- routes/services.py
+++ routes/services.py
@@ -1,25 +1,24 @@
 import shelve
 from flask import Blueprint, render_template, request, url_for, flash, redirect, Flask
-
 service = Blueprint('service', __name__)
 
 app = Flask(__name__)
 
 
-@app.route('/services')
+@service.route('/services')
 def services():
     return render_template('services/services.html')
 
 
-@app.route('/addNewService')
-def add_new_service():
-    services_dict = {}
-    db = shelve.open('services.db', 'r')
-    services_dict = db['services.db']
+@service.route('/retrieve_appointment')
+def retrieve_appointment():
+    appointment_dict = {}
+    db = shelve.open('appointment.db', 'r')
+    appointment_dict = db['services.db']
     db.close()
 
-    service_list = []
-    for key in services_dict:
-        services = services_dict,get(key)
-        service_list.append(services)
+    appointment_list = []
+    for key in appointment_dict:
+        appointment = appointment_dict.get(key)
+        appointment_list.append(services)
     return render_template('add_new_service.html')
