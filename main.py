846eb0d2eb09a1645a5f8bcb3bcb3b2478578517
155pbones <brianyuk@gmail.com>
2023-02-12 06:04:53 +0800

    updated repository

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ app = Flask(__name__)
 
 # app config
 app.config["SECRET_KEY"] = "641z69bc491f8cb891fc0417d2eb29bb5"
-app.config["PRODUCT_UPLOAD"] = 'static/media/images/product'
 app.config["PRODUCT_REVIEW_UPLOAD"] = 'static/media/images/reviews/product_reviews'
 
 # registered blueprints to app
